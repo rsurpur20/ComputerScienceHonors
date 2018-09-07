18392443127a70d1a3b28d20f88e2851b378b75f
Roshni_Surpur <rsurpur20@choate.edu>
2018-09-07 13:34:15 -0400

    Initial commit

---
+++ introduction.py
@@ -0,0 +1,6 @@
+print ("hello world")
+favpet=input("what is your favorite pet? \n")
+print("i love %s too!"%(favpet)) 
+username=input("what is your name? \n")
+print("hello " + username + "!")
+#using the module % and the +...+ is the same
