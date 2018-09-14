0b7c1fd9be74e7d2e509e7ca944e5ce2f4d9f876
Roshni_Surpur <rsurpur20@choate.edu>
2018-09-14 11:31:26 -0400

    final version

--- compoundedinterest.py
+++ compoundedinterest.py
@@ -2,7 +2,7 @@
 # 9/14/2018
 #
 # Compose a program that calculates and writes the amount of money you would have if you invested it
-# as a given interest rate compounded continuously, taking the number of years t, the principal P, and
+# as a given interest rate compounded continuously, taking the number of years t, the principle P, and
 # the annual interest rate r as commmand-line arguments. The desired value is given by the formula:
 # Pe^rt
 # Note that the e in the formula is the mathematical constant e.
@@ -11,7 +11,7 @@ import sys
 import math
 import datetime
 
-principal=float(sys.argv[1])
+principle=float(sys.argv[1])
 r=float(sys.argv[2])
 year=float(sys.argv[3])
 
@@ -23,7 +23,7 @@ x = datetime.datetime.now() #gets users current year
 
 
 print("Detected Arguments:")
-print("$"+str(principal))
+print("$"+str(principle))
 print(str(r)+"%")
 print("Current Year:" +str(x.year))
 print("End Year:" + str(year))
@@ -31,5 +31,5 @@ print("End Year:" + str(year))
 
 r1=r/100
 year=year-x.year
-result=principal*(math.e**(r1*year))
+result=principle*(math.e**(r1*year))
 print("\nCompounded Interest: $" +str(result))
