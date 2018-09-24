0f2c94e7c6c777e14c56d87c1f7d805c86664eeb
Roshni_Surpur <rsurpur20@choate.edu>
2018-09-23 22:23:41 -0400

    commented code

--- game.py
+++ game.py
@@ -2,45 +2,105 @@
 
 # rock paper scissors
 import random
-options=["rock", "paper","scissors"]
-computerchoice=random.choice(options)
-# print(computerchoice)
-def paper(userchoice,computerchoice):
-    if userchoice=="rock":
-        print("you lose")
-    elif userchoice=="scissors":
-        print("you win")
-    else:
-        print("Tie!")
-def scissors(userchoice,computerchoice):
-    if userchoice=="paper":
-        print("you lose")
-    elif userchoice=="rock":
-        print("you win")
-    else:
-        print("Tie!")
-def rock(userchoice,computerchoice):
-    if userchoice=="scissors":
-        print("you lose")
-    elif userchoice=="paper":
-        print("you win")
-    else:
-        print("Tie!")
-def playgame(userchoice):
-    print("User choose:",userchoice)
-    print("Computer choose:",computerchoice)
-    if computerchoice=="paper":
-        paper(userchoice,computerchoice)
-    elif computerchoice=="scissors":
-        scissors(userchoice,computerchoice)
-    elif computerchoice=="rock":
-        rock(userchoice,computerchoice)
-def ask():
-    userchoice=str.lower(input("Choose 'rock','paper','scissors'. \n"))
-    if userchoice in ("rock","scissors","paper"):
-        playgame(userchoice)
-    else:
-        print("I did not understand you answer.")
-        ask()
+def rockpaperscissors():
+    options=["rock", "paper","scissors"]
+    computerchoice=random.choice(options)
+    # print(computerchoice)
+    def paper(userchoice,computerchoice):
+        if userchoice=="rock":
+            print("you lose")
+        elif userchoice=="scissors":
+            print("you win")
+        else:
+            print("Tie!")
+    def scissors(userchoice,computerchoice):
+        if userchoice=="paper":
+            print("you lose")
+        elif userchoice=="rock":
+            print("you win")
+        else:
+            print("Tie!")
+    def rock(userchoice,computerchoice):
+        if userchoice=="scissors":
+            print("you lose")
+        elif userchoice=="paper":
+            print("you win")
+        else:
+            print("Tie!")
+    def playgame(userchoice):
+        print("User choose:",userchoice)
+        print("Computer choose:",computerchoice)
+        if computerchoice=="paper":
+            paper(userchoice,computerchoice)
+        elif computerchoice=="scissors":
+            scissors(userchoice,computerchoice)
+        elif computerchoice=="rock":
+            rock(userchoice,computerchoice)
+    def ask():
+        userchoice=str.lower(input("Choose 'rock','paper','scissors'. \n"))
+        if userchoice in ("rock","scissors","paper"):
+            playgame(userchoice)
+        else:
+            print("I did not understand you answer.")
+            ask()
 
-ask()
+    ask()
+def cardgame():
+    num={
+        1:1,
+        2:2,
+        3:3,
+        4:4,
+        5:5,
+        6:6,
+        7:7,
+        8:8,
+        9:9,
+        10:10,
+        11:"J",
+        12:"Q",
+        13:"K"
+    }
+    # print(random.choice(list(num.values())))
+    sign=["spades","diamonds","clover","hearts"]
+
+    computernumber=random.choice(list(num.keys()))
+    # print(num.values())
+    computersign=random.choice(sign)
+    usernumber=random.choice(list(num.keys()))
+    usersign=random.choice(sign)
+    # print(computernumber)
+    # print(usernumber)
+    if computernumber>usernumber:
+        print("Computer: %s%s"%(num[computernumber],computersign))
+        print("User: %s%s"%(num[usernumber],usersign))
+        print("you lose")
+
+    elif computernumber<usernumber:
+        print("Computer: %s%s"%(num[computernumber],computersign))
+        print("User: %s%s"%(num[usernumber],usersign))
+        print("you win")
+
+
+    elif computernumber==usernumber:
+        print("tie")
+        print("Computer: %s%s"%(random.choice(list(num.values())),computersign))
+        print("User: %s%s"%(random.choice(list(num.values())),usersign))
+    else:
+        print("error")
+
+def start():
+    game=input("Which game would you like to play?\n Type 1 for a cardgame. Type 2 for Rock, Paper, Scissors.")
+    if game in ("1","2"):
+        gameinput=int(game)
+        if gameinput==1:
+            cardgame()
+        elif gameinput==2:
+            rockpaperscissors()
+        else:
+            print("Error Try Again")
+            start()
+    else:
+        print("Did not Understand. Try Again.")
+        start()
+start()
--- tictactoe.py
+++ tictactoe.py
@@ -1,36 +1,102 @@
 import random
 
-print("_|_|_ \n_|_|_ \n_|_|_ \n")
+# print("_|_|_ \n_|_|_ \n_|_|_ \n")
 values=["_","_","_","_","_","_","_","_","_"]
 # print(values)
 line1=values[0]+"|"+values[1]+"|"+values[2]
 line2=values[3]+"|"+values[4]+"|"+values[5]
 line3=values[6]+"|"+values[7]+"|"+values[8]
+print("\nFor reference: the number you enter corresponds with these places:\n1|2|3\n4|5|6\n7|8|9")
+def win(line1,line2,line3,values):
+    if values[2]=="X" and values[4]=="X" and values[6]=="X":
+        print("You win")
+        exit()
+    if values[0]=="X" and values[3]=="X" and values[6]=="X":
+        print("You win")
+        exit()
+    if values[0]=="X" and values[1]=="X" and values[2]=="X":
+        print("You win")
+        exit()
+    if values[0]=="X" and values[4]=="X" and values[8]=="X":
+        print("You win")
+        exit()
+    if values[2]=="X" and values[5]=="X" and values[8]=="X":
+        print("You win")
+        exit()
+    if values[6]=="X" and values[7]=="X" and values[8]=="X":
+        print("You win")
+        exit()
+    if values[1]=="X" and values[4]=="X" and values[7]=="X":
+        print("You win")
+        exit()
+    #
+    if values[2]=="O" and values[4]=="O" and values[6]=="O":
+        print("You lose")
+        exit()
+    if values[0]=="O" and values[3]=="O" and values[6]=="O":
+        print("You lose")
+        exit()
+    if values[0]=="O" and values[1]=="O" and values[2]=="O":
+        print("You lose")
+        exit()
+    if values[0]=="O" and values[4]=="O" and values[7]=="O":
+        print("You lose")
+        exit()
+    if values[2]=="O" and values[5]=="O" and values[8]=="O":
+        print("You lose")
+        exit()
+    if values[6]=="O" and values[7]=="O" and values[8]=="O":
+        print("You lose")
+        exit()
+    if values[1]=="O" and values[4]=="O" and values[7]=="O":
+        print("You lose")
+        exit()
+
 def play(line1,line2,line3,values):
+    if "_" in values: #while there still is a spot that hasnt been takem
+    # making sure user puts in a whole number between 1-9
+        user=input("\nchoose a location: 1,2,3,4,5,6,7,8,9 \n")
+        if user in("1","2","3","4","5","6","7","8","9"):
+            userinput=int(user)-1
+            computerinput=(random.randint(0,8)-1)# setting random computer value
+            win(line1,line2,line3,values) #check if winning
+            if userinput==computerinput: #if both values are the same, reset the computer value
+                print("userinput==computerinput")
+                computerinput=(random.randint(0,8)-1)
+            if values[computerinput]!="_": #this makes sure computer doesn't use the same spot twice
 
-    user=int(input("choose a location"))
-    userinput=user-1
-    computerinput=(random.randint(0,8)-1)
-    if values[userinput]=="_":
-        values[userinput]="X"
-        values[computerinput]="O"
-        line1=values[0]+"|"+values[1]+"|"+values[2]
-        line2=values[3]+"|"+values[4]+"|"+values[5]
-        line3=values[6]+"|"+values[7]+"|"+values[8]
-        print(line1)
-        print(line2)
-        print(line3)
-        return(line1)
-        return(line2)
-        return(line3)
-        if userinput==computerinput:
-            print("userinput==computerinput")
-            computerinput=(random.randint(0,8)-1)
+                computerinput=(random.randint(0,8)-1)
+                play(line1,line2,line3,values)
+            if values[userinput]=="_" and values[computerinput]=="_":#if the space is empty
+
+                    # play(line1,line2,line3,values)
+                    # win(line1,line2,line3,values)
+
+                if userinput!=computerinput: #if the space is empty and the values arent equal play the game
+
+                    values[userinput]="X"
+                    values[computerinput]="O"
+                    line1=values[0]+"|"+values[1]+"|"+values[2]
+                    line2=values[3]+"|"+values[4]+"|"+values[5]
+                    line3=values[6]+"|"+values[7]+"|"+values[8]
+                    print(line1)
+                    print(line2)
+                    print(line3)
+                    return(line1)
+                    return(line2)
+                    return(line3)
+                    # win(line1,line2,line3,values)
+
+            else: #if space is taken
+                print("That space is already taken. Try Again.")
+                play(line1,line2,line3,values)
+        else: #if user doesnt enter the correct kind of number
+            print("Enter one of these numbers: 1,2,3,4,5,6,7,8,9")
             play(line1,line2,line3,values)
+    else:# if there are no more empty spaces
+        print("Game Over")
+        exit()
 
-    else:
-        print("That space is already taken. Try Again.")
-        play(line1,line2,line3,values)
     # what i need to do now:
     # make sure user doesnt choose the spot the computer already choose
     # winning mechanism
@@ -50,3 +116,4 @@ while i<90:
     # line1=values[0]+"|"+values[1]+"|"+values[2]
     play(line1,line2,line3,values)
     i+=1
+    win(line1,line2,line3,values)
