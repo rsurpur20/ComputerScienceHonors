c870de3f027cc5a0476951874ae2d3f8497dc548
Roshni_Surpur <rsurpur20@choate.edu>
2018-11-05 15:09:31 +0800

    put more questions

--- classes/deck.py
+++ classes/deck.py
@@ -5,20 +5,21 @@ from card import Card
 import random
 suits=["spades","clubs","diamonds","hearts"]
 class Deck:
+    # do I need all these self.'s?
     def __init__(self):
         self.decklist=[]
         self.i=0
         for i in range(1,14):
             # a=str(Card(i, "spades"))
-            self.decklist.append(Card(i, "spades"))
-            self.decklist.append(Card(i, "hearts"))
-            self.decklist.append(Card(i, "diamonds"))
-            self.decklist.append(Card(i, "clubs"))
+            self.decklist.append(Card(self.i, "spades"))
+            self.decklist.append(Card(self.i, "hearts"))
+            self.decklist.append(Card(self.i, "diamonds"))
+            self.decklist.append(Card(self.i, "clubs"))
             self.i=self.i+1
             # so now my deck of cards is made right?
         # return decklist
     def takerandomcard(self,rank,suit):
-        self.decklist.remove(Card(rank, str(suit)))
+        self.decklist.remove(Card(self.rank, str(self.suit)))
         return decklist
 
     def __str__(self):
